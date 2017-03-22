16fb9ebcc9f75a3ae37a0641346304541de49bd5
MariusTBjercke <marius@bjerckemedia.no>
2017-03-22 23:42:24 +0100

    Fixed indent in index.php

--- wp-content/themes/wpbootstrap/index.php
+++ wp-content/themes/wpbootstrap/index.php
@@ -1,31 +1,32 @@
 <?php
 get_header();
 ?>
-    <?php
-    if (get_the_title() === 'Nyheter') {
-        include('nyheter.php');
-    } else if (get_the_title() === 'Om oss') { ?>
-        <div class="container">
-            <div class="row">
+<?php
+if (get_the_title() === 'Nyheter') {
+    include('nyheter.php');
+} else if (get_the_title() === 'Om oss') {
+    ?>
+    <div class="container">
+        <div class="row">
             <?php
             $content = apply_filters('the_content', $post->post_content);
             echo $content;
             include('omoss.php');
             ?>
         </div>
-        </div>
-        <?php
+    </div>
+    <?php
     } else {
-        ?>
-        <div class="container">
-            <div class="row">
+    ?>
+    <div class="container">
+        <div class="row">
             <?php
             $content = apply_filters('the_content', $post->post_content);
             echo $content;
             ?>
-            </div>
         </div>
-        <?php
+    </div>
+    <?php
     }
     ?>
 <?php
