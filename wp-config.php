0affa7ce4f8e008c1a17672afce155d51e7aecd9
Marius Bjercke <marius@coretrek.no>
2017-04-27 21:42:30 +0200

    Changed htaccess and config

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'bjerckemedia_no_matoghelse');
+define('DB_NAME', 'matoghelse');
 
 /** MySQL database username */
-define('DB_USER', 'bjerckemedia_no_matoghelse');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Desktopmatoghelse94');
+define('DB_PASSWORD', '20C71900a920C');
 
 /** MySQL hostname */
-define('DB_HOST', 'bjerckemedia.no.mysql');
+define('DB_HOST', 'home.bjerckemedia.no');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
